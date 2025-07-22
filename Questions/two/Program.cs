bcebbdba1fcaae29be07094d982e0859879f52c0
Rupak Pandey <162466719+Rupak321@users.noreply.github.com>
2025-07-22 20:12:04 +0545

    Add files via upload

---
+++ Questions/two/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Two
+{
+    static void Main(string[] args)
+    {
+       
+       
+             System.Console.WriteLine("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                System.Console.WriteLine("The number is positive");
+            }
+            else if (num < 0)
+            {
+                System.Console.WriteLine("The number is negative");
+            }
+            else
+            {
+                System.Console.WriteLine("The number is zero");
+            }
+        }
+    }
+
