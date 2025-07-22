7868208ff2f4c3fb677d4d2804c93a13dc77bd01
Rupak Pandey <162466719+Rupak321@users.noreply.github.com>
2025-07-22 20:12:31 +0545

    Add files via upload

---
+++ Questions/Three/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Third
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter your name and address");
+        string name = Console.ReadLine();
+        string add = Console.ReadLine();
+        System.Console.WriteLine($"Hey {name} you live in {add}  ");
+
+    }
+}
