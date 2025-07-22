7fbba7f3a8bfbadf141dd775b4201b78d11dacc6
Rupak Pandey <162466719+Rupak321@users.noreply.github.com>
2025-07-22 20:02:08 +0545

    Add files via upload

---
+++ Number guessing game/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+class New
+{
+    static void Main(String[] args)
+    {
+        Random rand = new Random();
+        int secret = rand.Next(1, 101);
+        int attempts = 5;
+        bool guessed = false;
+
+
+
+
+        Console.WriteLine("Number Guessing Game");
+        Console.WriteLine("Guess a nummber in between 1 to 100(YOu have 5 chances)");
+        for (int i = 1; i <= attempts; i++)
+        {
+            Console.WriteLine($"Attempt no= {i}");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            if (guess == secret)
+            {
+                Console.WriteLine("Congratulation u guessed it right");
+                guessed = true;
+                break;
+
+            }
+            else if (guess < secret)
+            {
+                Console.WriteLine("You were too closed");
+            }
+            else if (guess > secret)
+            {
+                Console.WriteLine("Too low");
+            }
+           
+        }
+            if (!guessed)
+            {
+                Console.WriteLine($"yOu losed and ran out of attempts the number was {secret}");
+            }
+        
+    }
+}
