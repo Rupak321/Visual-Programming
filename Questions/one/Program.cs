bc5ffa068701781def10b8dd15d08742d7a08a07
Rupak Pandey <162466719+Rupak321@users.noreply.github.com>
2025-07-22 20:11:16 +0545

    Add files via upload

---
+++ Questions/one/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class One
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter a number");
+        int num = Convert.ToInt32(Console.ReadLine());
+        
+    }
+}
